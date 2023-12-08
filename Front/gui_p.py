6e88097a0bcb77ab37cb3c73df4d6d69c3c56c65
SelvinHernandez <charlyyuman@gmail.com>
2023-12-07 23:49:03 -0600

    se agrea boton run script, run sql y editor

--- Front/gui_p.py
+++ Front/gui_p.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 from util.generic import GENERIC
 class GUI_P:
     def __init__(self) :
+        self.notebook=None
+        self.mat_text=[]
         utl= GENERIC()
         self.ventana=tk.Tk()
         self.ventana.title('SQLX')
@@ -27,7 +29,7 @@ class GUI_P:
         #frame_botones
         frame_botones = tk.Frame(frame_menus,height = 50, bd=0, relief=tk.SOLID,bg='black')
         frame_botones.pack(side="top",fill=tk.X)
-        
+        #botones
         btn_archivo= tk.Menubutton(frame_botones,text="Archivo")
         archivo_ops=tk.Menu(btn_archivo)
         archivo_ops.add_command(label="Abrir")
@@ -41,17 +43,47 @@ class GUI_P:
         btn_tools=tk.Menubutton(frame_botones,text="Herramientas")
         tools_ops=tk.Menu(btn_tools)
         tools_ops.add_command(label="Bases de Datos")
-        tools_ops.add_command(label="SQL")
+        tools_ops.add_command(label="SQL",command=self.tools_sql)
         tools_ops.add_command(label="Exportar")
         tools_ops.add_command(label="Importar")
         btn_tools.config(menu=tools_ops)
         btn_tools.pack(side="left",padx=10,pady=10)
-        
+
+        btn_runscript=tk.Button(frame_botones,text="Run Script",command=self.run_script)
+        btn_runscript.pack(side="left",padx=10,pady=10)
+
+        btn_run_sql=tk.Button(frame_botones,text="Run SQL",command=self.run_sql)
+        btn_run_sql.pack(side="left",padx=10,pady=10)
+
+        #frame_form_fill
+        self.frame_form_fill = tk.Frame(frame_menus,height = 50,  bd=0, relief=tk.SOLID,bg='#fcfcfc')
+        self.frame_form_fill.pack(side="bottom",expand=tk.YES,fill=tk.BOTH)
 
         #title = tk.Label(frame_botones, text="Inicio de sesion",font=('Times', 30), fg="#666a88",bg='#fcfcfc',pady=50)
         #title.pack(expand=tk.YES,fill=tk.BOTH)
         self.ventana.mainloop()
 
 
-        
+    def tools_sql(self):
+        if self.notebook is None:
+            self.notebook=ttk.Notebook(self.frame_form_fill)
+            self.notebook.pack()
+        tab=ttk.Frame(self.notebook)
+        self.notebook.add(tab,text="Query"+str(len(self.notebook.tabs())+1))
+        print("size note: "+str(len(self.notebook.tabs())))
+        texto=tk.Text(tab)
+        texto.pack()
+        self.mat_text.append(texto)
+
+    def run_script(self):
+        index=self.notebook.index("current")
+        #texto=self.mat_text[index].get()
+        print("index: ",index,"- size mat_text: ",len(self.mat_text),self.mat_text[index].get("1.0", "end-1c"))
+
+    def run_sql(self):
+        index=self.notebook.index("current")
+        cursor_posicion=self.mat_text[index].index(tk.INSERT)
+        fila=cursor_posicion.split(".")[0]
+        linea_actual=self.mat_text[index].get(f"{fila}.0", f"{fila}.end-1c")
+        print("Texto en la línea actual:", linea_actual)
 
--- Front/test.py
+++ Front/test.py
@@ -1,23 +1,27 @@
 import tkinter as tk
 
-root = tk.Tk()
+def obtener_linea_actual():
+    # Obtener la posición del cursor
+    cursor_posicion = text_widget.index(tk.INSERT)
 
-# Creamos un panel
-panel = tk.Frame(root)
+    # Extraer la parte de la fila de la posición
+    fila = cursor_posicion.split(".")[0]
 
-# Colocamos el panel en la derecha
-panel.pack(side="right")
+    # Obtener el texto de la línea actual
+    linea_actual = text_widget.get(f"{fila}.0", f"{fila}.end-1c")
+    print("Texto en la línea actual:", linea_actual)
 
-# Agregamos una etiqueta al panel
-label = tk.Label(panel, text="Etiqueta")
-label.pack()
+# Crear la aplicación principal
+app = tk.Tk()
+app.title("Ejemplo de Texto - Línea Actual")
 
-# Agregamos un cuadro de texto al panel
-entry = tk.Entry(panel)
-entry.pack()
+# Crear un widget Text
+text_widget = tk.Text(app, height=5, width=30)
+text_widget.pack(padx=10, pady=10)
 
-# Agregamos una lista al panel
-listbox = tk.Listbox(panel)
-listbox.pack()
+# Botón para obtener la línea actual
+boton_obtener_linea = tk.Button(app, text="Obtener Línea Actual", command=obtener_linea_actual)
+boton_obtener_linea.pack(pady=10)
 
-root.mainloop()
+# Iniciar el bucle principal de la aplicación
+app.mainloop()
