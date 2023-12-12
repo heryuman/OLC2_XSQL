18ea13b8d568bf39ce44bb08a8de631ec1d38625
SelvinHernandez <charlyyuman@gmail.com>
2023-12-12 01:25:27 -0600

    se genera la clase que escribe el xml

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from Front.gui_p import GUI_P
-GUI_P()
+from util.manipulador_xml import CREATE_XML
+from util.table import TBL
+from util.column import COLUM
+
+creardb=CREATE_XML()
+#creardb.create_db("db_test4")
+
+tabla1=TBL("db_test3","my_tab1")
+columna1=COLUM("ID","INT",True,None,11)
+tabla1.insert_column(columna1)
+creardb.insert_table(tabla1)
+
+
+#GUI_P()
---
+++ util/column.py
@@ -0,0 +1,7 @@
+class COLUM:
+    def __init__(self,cname,tipo,pk,col_ref,col_size):
+        self._column_name=cname
+        self._type=tipo
+        self._pk=pk
+        self._col_fer=col_ref
+        self._col_size=col_size
