98e84a45e745d08b1902e2dc19f92047e7196f59
ELDER ARIEL LOPEZ SAMOL <elder.lopezs@claro.com.gt>
2023-12-12 22:06:10 -0600

    Funciones de Sistema al 15%

---
+++ FuncionesDeSistema/Fun_Suma.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Fun_Suma:
+    def __init__(self):
+        self._result: float = None
+
+    def getSuma (self, lista: List[float])-> float:
+
+        self._result = 0.0
+
+        return self._result
+        
