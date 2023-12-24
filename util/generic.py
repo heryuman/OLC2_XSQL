1159dfda2a0dc54f6541103ffbc28e76eaf4095e
SelvinHernandez <charlyyuman@gmail.com>
2023-12-24 05:09:42 -0600

    se agrega metodo INSERT

--- util/generic.py
+++ util/generic.py
@@ -8,3 +8,12 @@ class GENERIC:
         x = int((pantall_ancho/2) - (aplicacion_ancho/2))
         y = int((pantall_largo/2) - (aplicacion_largo/2))
         return ventana.geometry(f"{aplicacion_ancho}x{aplicacion_largo}+{x}+{y}")
+    
+class INSERT:
+    def __init__(self,db_name,tb_name,l_into,lvalues):
+        self._db_name=db_name #Nombre de la base de datos donde se insertara
+        self._tb_name=tb_name #Nombre de la tabla a insertar
+        self._lvalues=lvalues  #Lista de valores a insertar
+        self._linto=l_into     # Lista de columnas de la tabla 
+                              # INSERT INTO (col1,col2,col3)VALUES(val1,val2,val3)
+    
