0f24c159cf86a4d5a7119f93e767bbf126c9bac5
ELDER ARIEL LOPEZ SAMOL <elder.lopezs@claro.com.gt>
2023-12-28 19:35:55 -0600

    validacion de existencia de la base de datos en el comando use

--- Instrucciones/Use.py
+++ Instrucciones/Use.py
@@ -1,15 +1,19 @@
 from Abstract.Instruccion import Instruccion
 from Arbol.Nodo import Nodo
 from Simbolo.Ambito import Ambito
+from util.manipulador_xml import CREATE_XML
 
 class Use(Instruccion):
     def __init__(self, db_name,fila, colum):
         super().__init__(fila, colum)
         self.db_name = db_name
+        self.manipulador:CREATE_XML = CREATE_XML()
 
     def compilar(self, tree, tablaSim: Ambito, nodo: Nodo, output: []):
         #return super().compilar(tree, tablaSim, nodo, output)
-
+        if not self.manipulador.exist_db(self.db_name):
+            output.append(self.env.dbNotFound)
+            return
         nodo._token = "USE"
         nodo._lexema = "use"
         nodo._linea = self.fila
